53d6b6945a421bc1a56ed0dd863ebadb3ef92ba8
wasikurosaki <washikur.rahman@g.bracu.ac.bd>
2024-12-29 20:14:56 +0600

    minor updates and code cleaning

--- src/routes/graphqlRoutes.js
+++ src/routes/graphqlRoutes.js
@@ -3,12 +3,11 @@ const { graphqlHTTP } = require("express-graphql");
 const schema = require("../schema/schema"); // Path to your GraphQL schema
 const router = express.Router();
 
-// Route to handle GraphQL requests
 router.use(
   "/graphql", // GraphQL endpoint
   graphqlHTTP({
-    schema: schema, // Use the schema defined in the GraphQL schema file
-    graphiql: true, // Enable GraphiQL interface for testing in the browser
+    schema: schema,
+    graphiql: true,
   })
 );
 
